ed9d92e960050c75b32ed3fa03190c908de7c782
ghoz94 <ghoz94@tamu.edu>
2016-03-04 20:09:08 +0000

    test3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,9 +16,8 @@ class MoviesController < ApplicationController
     # based off sort_type, sort accordingly
     if params[:sort_type] == "title"
       @movie_highlight = "hilite"
-      @movies = Movie.order("title asc")
-      #Movie.order(:title)
-     # => SELECT "movies".* FROM "movies" ORDER BY title
+      #@movies = Movie.order("title asc")
+      Movies.order(:title) => SELECT "movies".* FROM "movies" ORDER BY "movies"."title" ASC
     elsif params[:sort_type] == "date"
       @movie_highlight = "hilite"
       @movies = Movie.order("release_date asc")
