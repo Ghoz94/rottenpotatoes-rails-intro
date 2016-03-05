e72dd3ea7692294d494fa95fbfa36597526c12db
ghoz94 <ghoz94@tamu.edu>
2016-03-05 01:48:36 +0000

    test2 p2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -33,10 +33,10 @@ class MoviesController < ApplicationController
     # based off sort_type, sort accordingly using ActiveRecord order
     if session[:sort_type] == "title"
       @hltitle = "hilite"
-      @movies = Movie.order(:title)
+      @movies = Movie.order(:title).select(:rating).map(:rating).uniq
     elsif session[:sort_type] == "date"
       @hldate = "hilite"
-      @movies = Movie.order(:release_date)
+      @movies = Movie.order(:release_date).select(:rating).map(:rating).uniq
       #@movies = @movies.sort! { |a,b| a.release_date <=> b.release_date }
     end
   end
